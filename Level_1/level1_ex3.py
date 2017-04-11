6ce8932ac1178dc8b165e49f7c6beb9d7c68dfc0
Cristian <cristianalemanfuentes@gmail.com>
2017-04-10 19:14:51 -0500

    :)))

--- Level_1/level1_ex3.py
+++ Level_1/level1_ex3.py
@@ -8,3 +8,19 @@ Use __init__ method to construct some parameters
 
 
 """
+class strup:
+	def __init__(self,cadena):
+		self.cadena=cadena
+
+	def getString(self):
+		s = input()
+		self.cadena=s
+		return s
+
+	def printString(self):
+		return self.cadena.upper()
+
+if __name__== '__main__':
+	obj = strup("")
+	obj.getString()
+	print(obj.printString())
