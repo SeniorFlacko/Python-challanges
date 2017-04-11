960e2a8d214d70db718db40f4805db9cadb3b580
Cristian <cristianalemanfuentes@gmail.com>
2017-04-10 20:43:52 -0500

    ?

--- Level_1/level1_ex5.py
+++ Level_1/level1_ex5.py
@@ -11,3 +11,13 @@ The sequence looks like this: 1, 1, 2, 3, 5, 8, 13,
 
 
 """
+
+def fib(n):
+	lista = [0,1]
+	for i in range(2,n):
+		lista.append(lista[-1] + lista[-2])
+	return lista
+
+if __name__ == '__main__':
+	n = int(input('How many fibonacci numbers: '))
+	print(fib(n))
